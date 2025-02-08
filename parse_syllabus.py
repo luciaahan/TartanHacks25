0da661364a3e5c32fcdc2849de03053443a7212d
Lucia Han <jeongwo2@andrew.cmu.edu>
2025-02-07 22:52:37 -0500

    yes

---
+++ parse_syllabus.py
@@ -0,0 +1,61 @@
+from openai import OpenAI
+import fitz
+from pydantic import BaseModel
+
+class core_info(BaseModel):
+  className: str
+  classNumber: str
+  lectureTime: str
+  hwDates: str
+  examDates: str
+  lateday: str
+  attendance: str
+  gradeWeight: str
+
+  
+def text_2_pdf(pdf_path):
+  doc = fitz.open(pdf_path)
+  text = '\n'.join([page.get_text("text") for page in doc])
+  return text
+
+def analyze_text(text):
+
+  client = OpenAI(
+    api_key="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"
+  )
+
+  system_prompt = '''
+  You will be provided with a text that is a syllabus of a course at Carnegie Mellon University. 
+  You will read through the document and give me the necessary information. 
+  For the return format, you have to follow these:
+  - className is course name 
+  - classNumber is course number
+  - lectureTime is the day and time of the lectures
+  - hwDates is due dates of all homeworks
+  - examDates is dates of all exams
+  - lateday is the policy for late assignments
+  - attendance is the policy for attending lectures and recitations
+  - gradeWeight is the weights and categories for each grading criteria
+  All information related to grade should be in gradeWeight, not in other categories.
+  '''
+  response = client.beta.chat.completions.parse(
+    model="gpt-4o-mini",
+    store=True,
+    messages=[
+      {"role": "system", "content": system_prompt},
+      {"role": "user", "content": text}
+    ],
+    response_format = core_info
+  )
+
+  return response.choices[0].message.parsed
+
+
+def main():
+  pdf_file = 'syllabus.pdf'
+  extracted_syllabus = text_2_pdf(pdf_file)
+  extracted_data = analyze_text(extracted_syllabus)
+  print(extracted_data.className)
+
+
+main()
